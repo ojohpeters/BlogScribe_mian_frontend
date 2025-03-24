b556be5baff77a0a8afef6b81f84b8182c5f9597
ojochegbe <peterspraisezy>
2025-03-24 13:47:29 +0000

    fixed endpoint in  password reset file

--- app/password-reset/page.tsx
+++ app/password-reset/page.tsx
@@ -39,7 +39,7 @@ export default function ResetPassword() {
     setIsSuccess(false)
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/users/request-reset-email/", {
+      const response = await fetch("https://blogbackend-crimson-frog-3248.fly.dev/api/users/request-reset-email/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
